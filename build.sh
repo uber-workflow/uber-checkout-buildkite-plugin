826e31537b26f17e70b6e8a6da82bd77693d498a
Albert Wu <wua@uber.com>
2021-01-14 17:07:10 -0800

    remove comments

--- build.sh
+++ build.sh
@@ -6,24 +6,3 @@
   env GOOS=darwin GOARCH=amd64 go build -o ../bin/uber-checkout-darwin-amd64 main.go
   env GOOS=linux GOARCH=amd64 go build -o ../bin/uber-checkout-linux-amd64 main.go
 )
-
-# readonly dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
-# if [ "$BUILDKITE" = true ]; then
-#   GOOS=linux
-#   GOARCH=amd64
-# else
-#   GOOS=$(go env GOOS)
-#   GOARCH=$(go env GOARCH)
-# fi
-
-# # build in docker image
-# docker run --rm \
-#   -v "$dir/src":/usr/src \
-#   -w /usr/src \
-#   -e GOOS="$GOOS" \
-#   -e GOARCH="$GOARCH" \
-#   golang:1.15-alpine \
-#   go build ./main.go
-
-# mv "${dir}/src/main" ./cli
